0206572c2cbc8390952ab4c41d6f5a10ce170f76
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-22 16:02:09 -0400

    Created a router and model file for the Articles table, and created table methods that fulfill necesarry CRUD operations

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
+
 const server = express();
+const db = require("../data/dbConfig.js")
+
 server.use(express.json());
+
+const articlesRouter = require("../articles/articles_router.js")
+
+server.use("/articles", articlesRouter);
 
 module.exports = server;
---
+++ articles/articles_model.js
@@ -0,0 +1,29 @@
+const knex = require("knex");
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+
+module.exports = {
+    find,
+    findById,
+    add,
+    remove
+}
+
+function find() {
+    return db("articles");
+}
+
+function findById(id) {
+    return db("articles").where({id: id});
+}
+
+function add(newArticle) {
+    return db("articles").insert(newArticle)
+    .then(() => {
+        return newArticle
+    })
+}
+
+function remove(id) {
+    return db("articles").del().where({id: id});
+}
