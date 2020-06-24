ba7788300a5799ef9c7a04d8d792b0b151597ace
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-24 16:20:14 -0400

    changed the production connection settings

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/pintereach";
-
+// const pgConnection = ;
+// `postgres://${process.env.DB_PRODUCTION_USER}:${process.env.DB_PRODUCTION_PW}@${process.env.DB_PRODUCTION_HOST}:${process.env.DB_PRODUCTION_PORT}/${process.env.DATABASE_PRODUCTION}`
 module.exports = {
 
   development: {
@@ -12,29 +12,35 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'pgConnection',
-      user:     'postgres',
-      password: 'GreatDays6617'
+    // connection: `postgres://${process.env.DB_PRODUCTION_USER}:${process.env.DB_PRODUCTION_PW}@${process.env.DB_PRODUCTION_HOST}:${process.env.DB_PRODUCTION_PORT}/${process.env.DATABASE_PRODUCTION}`,
+    connection: "postgres://gmcwvelwfqluqr:861e1178c1888eee75a59835649267028ef2b92ee941260d7c41e8676c9f68ae@ec2-52-200-48-116.compute-1.amazonaws.com:5432/d1qnjcia4l4qd5",
+    searchPath: ["public"],
+    options: {
+      schema: "public"
     },
+    // connection: {
+    //   database: 'pintereach',
+    //   user:     'postgres',
+    //   password: 'GreatDays6617'
+    // },
     pool: {
       min: 2,
       max: 10
--- users/user_articles_router.js
+++ users/user_articles_router.js
@@ -20,6 +20,9 @@ router.get("/", (req, res) => {
     .then(response => {
         res.status(200).json(response);
     })
+    .catch(err => {
+        res.status(500).json(err.message)
+    })
 })
 
 
